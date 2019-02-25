b70de31ca73afbe0643b4d70bec1da643995014a
shpaackle <shpaackle@gmail.com>
2019-02-25 18:49:26 -0500

    removed unnecessary dunder methods after adding dataclass decorator

--- src/map_objects/point.py
+++ src/map_objects/point.py
@@ -23,23 +23,11 @@ class Point:
     def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y)
 
-    # def __eq__(self, other):
-    #     return self.x == other.x and self.y == other.y
-
-    # def __ne__(self, other):
-    #     return self.x != other.x and self.y != other.y
-
-    # def __hash__(self):
-    #     return self.x ^ self.y
-
     def __mul__(self, other: int):
         return Point(self.x * other, self.y * other)
 
     def __str__(self):
         return f"({self.x}, {self.y})"
-
-    # def __repr__(self):
-    #     return f"({self.__class__.__name__}) x={self.x}, y={self.y}"
 
     def __iter__(self):
         yield self.x
